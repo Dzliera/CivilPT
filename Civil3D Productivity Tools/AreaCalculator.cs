837f5b7c2d0b2a4753837b900a4329d549e704bb
giorgi chxikvadze <giorgi_chxikva@outlook.com>
2019-09-06 17:38:50 +0400

    Minor changes

---
+++ Civil3D Productivity Tools/AreaCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace Civil3D_Productivity_Tools
+{
+    public static class AreaCalculator
+    {
+        public static void CalculateArea(Polyline2d pStart, Polyline2d pMiddle, Polyline2d pEnd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Point2d CalculateIntersectionPoint(Line2d line1, Line2d line2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double CalculateTriangleArea(Point2d p1, Point2d p2, Point2d p3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
